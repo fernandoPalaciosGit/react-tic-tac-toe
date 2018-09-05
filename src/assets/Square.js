3595b0e0a5abfdfece4572099554d3877a24a55e
Fernando Palacios Landi <fernandopalacioslandi@gmail.com>
2018-09-05 21:16:08 +0200

    onclick square print its index

--- src/assets/Square.js
+++ src/assets/Square.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import _ from 'lodash';
 
 class Square extends React.Component {
     render() {
         return (
-            <button className="square">
+            <button className="square"
+            onClick={_.partial(alert, this.props.index)}>
                 {this.props.index}
             </button>
         );
